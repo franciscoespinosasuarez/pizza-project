61e8f6064719622324730a4b198a336b9ca47d97
jemb4 <jesus8.mb@gmail.com>
2022-08-01 19:07:47 +0000

    deploy

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,30 +4,23 @@ import { PizzaCard } from "../component/pizzacard/pizzacard";
 import { FilterIngredient } from "../component/filteringredient/filteringredient";
 import Navbar from "../component/navbar";
 import config from "../config";
-import "../component/createpizza/createpizza.css"
+import "../component/createpizza/createpizza.css";
+import "../../styles/home.css";
 
+export const Home = () => {
+  const { store, actions } = useContext(Context);
 
+  // useEffect(() =>{
+  //     actions.getPizzas();
+  //     actions.filter_function();
+  // })
 
-export const Home = () =>{
-    const {store, actions } = useContext(Context);
+  //funciones:
+  const filtroPizza = () => {};
 
-    // useEffect(() =>{
-    //     actions.getPizzas();
-    //     actions.filter_function();
-    // })
-
-
-
-
-    //funciones:
-    const filtroPizza = () =>{
-
-    }
-
-    return(
-        <>
-        
-         {/* <!-- Modal --> */}
+  return (
+    <>
+      {/* <!-- Modal --> */}
       <div
         className="modal fade"
         id="staticBackdrop"
@@ -55,37 +48,37 @@ export const Home = () =>{
         </div>
       </div>
 
-        <Navbar />
-        
-        {/* {Mostrar modal} */}
-    <div className="container">
-      <div className="d-flex justify-content-center">
-            <button
-              type="button"
-              className="create-pizza-btn mb-3"
-              data-bs-toggle="modal"
-              data-bs-target="#staticBackdrop"
-            >
-              Elige los ingredientes
-            </button>
-          </div>
+      <Navbar />
 
-          <div className="container row selected py-2">
-            {store.itemArray.map((val, key) => {
-              console.log(val);
-              return (
-                <div className="container col-3 py-2 d-flex justify-content-center">
-                  <button
-                    className="ingredient-btn"
-                    type="button"
-                    onClick={() => actions.eliminate_ingredient(val)}
-                  >
-                    {val.first_name}
-                  </button>
-                </div>
-              );
-            })}
-          </div>
+      {/* {Mostrar modal} */}
+      <div className="container">
+        <div className="d-flex justify-content-center">
+          <button
+            type="button"
+            className="create-pizza-btn mb-3"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop"
+          >
+            Elige los ingredientes
+          </button>
+        </div>
+
+        <div className="container row selected py-2">
+          {store.itemArray.map((val, key) => {
+            console.log(val);
+            return (
+              <div className="container col-3 py-2 d-flex justify-content-center">
+                <button
+                  className="ingredient-btn"
+                  type="button"
+                  onClick={() => actions.eliminate_ingredient(val)}
+                >
+                  {val.first_name}
+                </button>
+              </div>
+            );
+          })}
+        </div>
         {/* mostrar información */}
         {/* <div> quitar comentario cuando se rellene el flux
                   {store.pizzas.length > 0 ? (
@@ -110,15 +103,13 @@ export const Home = () =>{
                     </div>
                   )}
                 </div> */}
-        <div className="row pt-5">        
-            <PizzaCard />
-            <PizzaCard />
-            <PizzaCard />
-            <PizzaCard />
+        <div className="row pt-5">
+          <PizzaCard />
+          <PizzaCard />
+          <PizzaCard />
+          <PizzaCard />
         </div>
-
-        </div>
-        </>
-
-    )
-}
+      </div>
+    </>
+  );
+};
